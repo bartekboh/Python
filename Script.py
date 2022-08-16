9192118bd5b9192b7c77e4b9ecac0c16648a5192
Bartek Bohdziewicz <bartekbohdziewicz@gmail.com>
2022-08-16 19:43:17 +0200

    main script

---
+++ Script.py
@@ -0,0 +1,26 @@
+
+admin = {
+  "log": "admin",
+  "pas": "admin",
+  "access": "full"
+}
+
+bartek = {
+  "log": "bartek123",
+  "pas": "bartek123",
+  "age": "17",
+  "height": "184"
+}
+
+log = input("Login:")
+pas = input("Password:")
+
+if log == admin["log"] and pas == admin["pas"]:
+  user = admin
+elif log == bartek["log"] and pas == bartek["pas"]:
+  user = bartek
+
+try:
+  print(user["log"])
+except:
+  print("Invalid login or password!")
