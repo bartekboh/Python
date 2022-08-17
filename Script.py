c87dc73b7fe6070766f0ccb297d45272f6aea041
Bartek Bohdziewicz <bartekbohdziewicz@gmail.com>
2022-08-17 18:07:03 +0200

    Implemented new way to save passes

--- Script.py
+++ Script.py
@@ -1,29 +1,32 @@
 
 admin = {
-  #"pas":("admin", "admin")
-  "log": "admin",
-  "pas": "admin",
-  "access": "full"
+  "pas":("admin", "admin"),
+  #"log": "admin",
+  #"pas": "admin",
+  "access": "full",
+  "name": "admin"
 }
 
 bartek = {
-  #"pas":("bartek123","bartek123")
-  "log": "bartek123",
-  "pas": "bartek123",
+  "pas":("bartek123","bartek123"),
+  #"log": "bartek123",
+  #"pas": "bartek123",
+  "access": "limited",
   "age": "17",
-  "height": "184"
+  "height": "184",
 }
 
 log = input("Login:")
 pas = input("Password:")
 
-if log == admin["log"] and pas == admin["pas"]:
+if log == admin["pas"][0] and pas == admin["pas"][1]:
   user = admin
-elif log == bartek["log"] and pas == bartek["pas"]:
+elif log == bartek["pas"][0] and pas == bartek["pas"][1]:
   user = bartek
 
 try:
-  print(user["log"])
-  #print(user["pas"[0]])
+  print("Hello " + user["pas"][0])
 except:
   print("Invalid login or password!")
+
+print("Your access is: " + user["access"])
