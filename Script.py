b3648b032996db961540f36ba23cceb810e49579
Bartek Bohdziewicz <bartekbohdziewicz@gmail.com>
2022-08-17 18:32:16 +0200

    log_in fuction

--- Script.py
+++ Script.py
@@ -1,28 +1,33 @@
-
 admin = {
-  "pas":("admin", "admin"),
+  "pas": ("admin", "admin"),
   "access": "full",
   "name": "admin"
 }
 
 bartek = {
-  "pas":("bartek123","bartek123"),
+  "pas": ("bartek123", "bartek123"),
   "access": "limited",
   "age": "17",
   "height": "184",
 }
 
-log = input("Login:")
-pas = input("Password:")
 
-if log == admin["pas"][0] and pas == admin["pas"][1]:
-  user = admin
-elif log == bartek["pas"][0] and pas == bartek["pas"][1]:
-  user = bartek
+def log_in():
+  log = input("Login:")
+  pas = input("Password:")
+
+  global user
+
+  if log == admin["pas"][0] and pas == admin["pas"][1]:
+    user = admin
+  elif log == bartek["pas"][0] and pas == bartek["pas"][1]:
+    user = bartek
+
+
+log_in()
 
 try:
   print("Hello " + user["pas"][0])
+  print("Your access is: " + user["access"])
 except:
   print("Invalid login or password!")
-
-print("Your access is: " + user["access"])
